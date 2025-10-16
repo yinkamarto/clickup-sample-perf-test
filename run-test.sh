ad76b8ad3f8f7c73a4686aa1d6f75edfe4c3bdd0
Yinka Martins <yinkamarto@gmail.com>
2025-10-16 08:20:11 -0400

    Resolve host address

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
+DOMAIN="web_service"
+
+function resolve_host {
+    echo -e "\nResolving host name"
+    if ping -c1 -W1 "$DOMAIN" &>/dev/null; then
+        echo "Host already written to /etc/hosts"
+    else
+        echo -e "127.0.0.1 \t$DOMAIN" >> /etc/hosts
+        echo "Successfully wrote to /etc/hosts"
+    fi
+    echo -e "\n"
+}
+
 # Build image and container
 docker compose up --build -d
+# Resolve host
+resolve_host
 # Execute locust test
 docker compose run --remove-orphans locust_service
 # Remove containers
